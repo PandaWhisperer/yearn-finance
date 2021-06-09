429cffedd978d5474fb7b7af3d5228b0e128a3e8
Christoph Wagner <github@pandawhisperer.net>
2021-06-08 22:09:53 -0700

    Add dotenv for easier setup in development

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
+  webpack: (config, { dev, isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
       config.node = {
         fs: 'empty'
       }
+    }
+
+    // load dotenv config in development
+    if (dev) {
+      const Dotenv = require('dotenv-webpack');
+      config.plugins.push(new Dotenv())
     }
 
     return config
